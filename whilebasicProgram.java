6f7df85270394373e5ff782329f832fc74e6e0ac
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-15 18:32:57 +0530

    Add files via upload

---
+++ whilebasicProgram.java
@@ -0,0 +1,18 @@
+package april;
+
+public class whilebasicProgram 
+{
+	public static void main(String []args)
+	{
+		int num=20;
+		while(num<=0)
+		{
+			if(num/2==0)
+			{
+				System.out.println(num);
+				
+			}
+		}
+	}
+
+}
