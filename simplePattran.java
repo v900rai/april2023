fb61702360a0c0d67f386601ccef11f4629de89a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-16 18:37:52 +0530

    Add files via upload

---
+++ simplePattran.java
@@ -0,0 +1,15 @@
+package april;
+
+public class simplePattran 
+{
+	public static void main(String []args) {
+	for(int row=1; row<5; row++ )
+	{
+		for(int col=row+1; col<5; col++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	}
+}
