f461b2099ee9ab59a72631ba9273d63cfa955ebc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-18 22:49:00 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,12 +1,33 @@
+package traget200;
 
-public class Person 
+public class Person
 {
-	int age=24; /// instance variable always time create the object
-	public static void main(String []rgs)
-	{
-		// create an object
-		Person rai=new Person();
-		System.out.println(rai.age);
-	}
+	
+	    String name,address;   
+	    int age;  
+	    public Person(int age, String name, String address)  
+	    {  
+	        this.age = age;  
+	        this.name = name;  
+	        this.address = address;  
+	    }  
+	}  
+	class Employee extends Person   
+	{  
+	    float salary;  
+	    public Employee(int age, String name, String address, float salary)  
+	    {  
+	        super(age,name,address);  
+	        this.salary = salary;  
+	    }  
+	}  
+     public class vishal //// constructor chaining
+	{  
+	    public static void main (String args[])  
+	    {  
+	        Employee e = new Employee(22, "Mukesh", "Delhi", 90000);  
+	        System.out.println("Name: "+e.name+" Salary: "+e.salary+" Age: "+e.age+" Address: "+e.address);  
+	    }  
+	}  
+        
 
-}
