cc54a6e9003070464ecdc1c8fb1f138748016204
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-19 23:09:05 +0530

    Add files via upload

--- MainOfClass.java
+++ MainOfClass.java
@@ -1,14 +1,24 @@
-package Employee;
+package edu.jspiders.heca48.Interface3;
 
 public class MainOfClass 
 {
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args) 
 	{
-		EmployeeFrist emp=new EmployeeFrist();
-		//emp.insertEmp();
-		emp.updateEmp();
+		Tata ta=new Tata();
+		System.out.println("===tata car======");
+		System.out.println(ta.colour());
+		System.out.println(ta.price());	
 		
+		System.out.println("====kia car=====");
 		
+		Kia ka=new Kia();
+		System.out.println(ka.colour());
+		System.out.println(ka.price());	
+		System.out.println("=====hyundai car====");
+		
+		Hyundai hu=new Hyundai();
+		System.out.println(hu.colour());
+		System.out.println(hu.price());	
 	}
 
 }
