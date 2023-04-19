e5d6c119fca57aa87753f627474ae528c93be7e2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-19 23:11:17 +0530

    Add files via upload

---
+++ Tata.java
@@ -0,0 +1,16 @@
+package edu.jspiders.heca48.Interface3;
+
+public class Tata implements Car
+{
+	public String colour()
+	{
+		return "black";
+		
+	}
+	public double price()
+	{
+		return 124554.33;
+		
+	}
+
+}
