71d3b07e8ba19dcd734d5af72822e1ec7f2a54a4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-19 23:03:17 +0530

    Add files via upload

---
+++ Hyundai.java
@@ -0,0 +1,16 @@
+package edu.jspiders.heca48.Interface3;
+
+public class Hyundai implements Car
+{
+	public String colour()
+	{
+		return "white";
+		
+	}
+	public double price()
+	{
+		return 234567.66;
+		
+	}
+
+}
