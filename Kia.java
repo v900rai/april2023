3312854480f31334a338b2fbd4b66cfb5bbeb05e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-19 23:03:41 +0530

    Add files via upload

---
+++ Kia.java
@@ -0,0 +1,16 @@
+package edu.jspiders.heca48.Interface3;
+
+public class Kia implements Car
+{
+	public String colour()
+	{
+		return "blue";
+		
+	}
+	public double price()
+	{
+		return 1234545.44;
+		
+	}
+
+}
