32c01dfee12043879ebee590f341aea963382fbf
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-04-23 23:32:24 +0530

    Add files via upload

---
+++ publicclassSingleInheritanceInJavaaaa.java
@@ -0,0 +1,19 @@
+package april;
+
+public class publicclassSingleInheritanceInJavaaaa 
+{
+	
+		 static int num1=10;
+		 static  int num2=5;
+	}
+
+	class MainInheritancee extends SingleInheritanceInJava{
+		public static void main(String[] args){
+		int num3=2;
+		String str="vishalRai";
+		int result=num1+num2+num3;
+		System.out.println("Result of child class is "+result);
+		}
+
+
+}
